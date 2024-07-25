d2ea25b89364cfb6d149ed5c51d2f6020fe3b4bd
Nirav <niravpatelp129@gmail.com>
2024-07-25 13:39:42 -0400

    * fix(server.js): removed comment about port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change this to any port you prefer
+const port = 3000;
 
 // Middleware
 app.use(express.json());
