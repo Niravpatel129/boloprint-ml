1b61e6cecc2c5bd404d97c4fd31435f6ab0d7fb6
Nirav <niravpatelp129@gmail.com>
2024-07-25 13:38:01 -0400

    * feat(server): added server.js file with basic setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change this to any port you prefer
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
